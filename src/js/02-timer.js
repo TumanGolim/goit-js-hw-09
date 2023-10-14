383b396313136130dbe48ead3c59010ee922a778
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2023-10-14 19:24:06 +0300

    task3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,10 +8,11 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    updateStartButtonState(selectedDates[0]);
   },
 };
 
-flatpickr('#datetime-picker', options);
+const datetimePicker = flatpickr('#datetime-picker', options);
 
 const startButton = document.querySelector('[data-start]');
 const daysElement = document.querySelector('[data-days]');
@@ -21,24 +22,43 @@ const secondsElement = document.querySelector('[data-seconds]');
 
 let countdownInterval;
 
+function updateStartButtonState(selectedDate) {
+  if (selectedDate instanceof Date && !isNaN(selectedDate)) {
+    startButton.disabled = false; 
+  } else {
+    startButton.disabled = true; 
+  }
+}
+
+updateStartButtonState(null);
+
 startButton.addEventListener('click', () => {
-  const selectedDate = flatpickr.parseDate(
-    document.getElementById('datetime-picker').value
-  );
+  const selectedDate = datetimePicker.selectedDates[0];
+
+  if (!selectedDate) {
+    alert('Будь ласка, виберіть дійсну дату і час.');
+    return;
+  }
 
   const updateTimer = () => {
     const timeRemaining = selectedDate - new Date();
 
     if (timeRemaining <= 0) {
       clearInterval(countdownInterval);
-      updateTimerDisplay(0);
-      alert('Countdown completed!');
+      updateTimerDisplay({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      });
+      alert('Зворотний відлік завершено!');
     } else {
       const time = convertMs(timeRemaining);
       updateTimerDisplay(time);
-      timeRemaining = 1000;
     }
   };
+
+  updateTimer();
 
   countdownInterval = setInterval(updateTimer, 1000);
 });
@@ -62,8 +82,8 @@ function convertMs(ms) {
 
   const days = Math.floor(ms / day);
   const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const minutes = Math.floor((ms % hour) / minute);
+  const seconds = Math.floor((ms % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
